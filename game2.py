11c6e4b60fe970621ae43923d437f133bdccc4d5
Marc Mascarell <marcmascarell@gmail.com>
2012-11-14 18:57:38 +0100

    Game in progress, added coins

---
+++ game2.py
@@ -0,0 +1,68 @@
+import pygame
+import random
+
+pygame.init()
+
+screen = pygame.display.set_mode((500,500))
+running = True
+time = pygame.time.Clock()
+
+la_font = pygame.font.SysFont("Courier", 20, True, False)
+
+info = la_font.render("Zombie Pyrty",0,(255,255,255))
+
+listarec = []
+r1 = pygame.Rect(0,0,1,1);
+
+secondsint = 0
+
+so = pygame.mixer.Sound("sound.wav")
+so.play(-1)
+
+last_divided = 0
+coins = 50
+
+for x in range(25):
+    w = random.randrange(10,30)
+    h = random.randrange(10,40)
+    x = random.randrange(450)
+    y = random.randrange(20,450)
+    listarec.append(pygame.Rect(x, y, w, h))
+
+while running == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for recs in listarec:
+                if r1.colliderect(recs):
+                    recs.width = 0
+                    recs.height = 0
+
+    time.tick(20)
+    print pygame.time.get_ticks()/1000
+    secondsint = pygame.time.get_ticks()/1000
+    seconds = str(secondsint)
+
+    contador = la_font.render(seconds,0,(255,255,255))
+
+    if (secondsint % 2) == 0 and secondsint > last_divided:
+        last_divided = secondsint
+        coins = coins + 50
+
+    show_coins = "$ " + str(coins)
+    show_coins = la_font.render(show_coins,0,(255,255,255))
+
+    r1.center = pygame.mouse.get_pos()
+
+    screen.fill((0,0,0))
+    screen.blit(info, (20, 10))
+    screen.blit(contador, (450, 10))
+    screen.blit(show_coins, (250, 10))
+
+    for recs in listarec:
+        pygame.draw.rect(screen,(0,200,0), recs)
+    pygame.draw.rect(screen,(200,20,20), r1)
+    pygame.display.update()
+
+pygame.quit()
