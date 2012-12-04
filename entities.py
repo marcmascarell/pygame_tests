dfbe5d02cc30c36386e50ed8f43ea77d18500f15
franki <francescortiz@gmail.com>
2012-12-04 19:40:41 +0100

    mockup funcional

---
+++ entities.py
@@ -0,0 +1,93 @@
+#-*- coding: UTF-8 -*-
+from math import sqrt
+import random
+import pygame
+from pygame.constants import SRCALPHA
+from pygame import transform
+
+__author__ = 'franki'
+
+
+class DisplayObject(object):
+    def __init__(self, screen, src_surface):
+        self.screen = screen
+        self.src_surface = src_surface
+        self.x = float(0)
+        self.y = float(0)
+        self.w = float(src_surface.get_width())
+        self.h = float(src_surface.get_height())
+        self.aspect_ratio = float(self.w) / float(self.h)
+        self.surface = pygame.Surface((self.w, self.h), SRCALPHA)
+        self.surface.blit(src_surface, (0, 0))
+
+    def render(self):
+        self.screen.blit(self.surface, (self.x, self.y))
+
+    def frame(self, cursor, left_click):
+        pass
+
+    def get_rect(self):
+        return self.x, self.y, self.w, self.h
+
+    def get_rect_int(self):
+        return int(round(self.x)), int(round(self.y)), int(round(self.w)), int(round(self.h))
+
+    def resize(self, scale):
+        self.w *= scale
+        self.h *= scale
+        self.surface = transform.scale(self.src_surface, (int(round(self.w)), int(round(self.h))))
+
+
+class Character(DisplayObject):
+    def __init__(self, *args, **kwargs):
+        super(Character, self).__init__(*args, **kwargs)
+        self.energy = 1
+        self.speed = 5
+
+    def kill(self):
+        pass
+
+    def hit(self, position):
+        hit_x = position[0] - self.x
+        hit_y = position[1] - self.y
+        self.energy -= 1
+        if self.energy:
+            pygame.draw.circle(
+                self.surface,
+                (200,0,0),
+                (int(round(hit_x)), int(round(hit_y))),
+                 5,
+                 1
+            )
+        else:
+            self.kill()
+
+    def frame(self, cursor, left_click):
+        if left_click and cursor.colliderect(self.get_rect()):
+            self.hit(cursor.center)
+
+        cursor_x = cursor.center[0]
+        cursor_y = cursor.center[1]
+
+        d = sqrt((self.x + self.w / 2 - cursor_x) ** 2 + (self.y + self.h / 2 - cursor_y) ** 2)
+        print d
+        self.speed = max(.1, 10 - d / 20)
+
+        self.x += self.speed if cursor_x < self.x + self.w / 2 else -self.speed
+        self.y += self.speed if cursor_y < self.y + self.h / 2 else -self.speed
+
+        self.x = max(0, min(self.screen.get_width() - self.w, self.x))
+        self.y = max(0, min(self.screen.get_height() - self.h, self.y))
+
+
+class Zombie(Character):
+
+    def __init__(self, *args, **kwargs):
+        super(Zombie, self).__init__(*args, **kwargs)
+        self.x = random.randrange(450)
+        self.y = random.randrange(20, 450)
+        self.resize(random.uniform(.5, 1))
+        self.energy = 3
+
+    def kill(self):
+        self.__init__(self.screen, self.src_surface)
--- game2.py
+++ game2.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from pygame import transform
+from entities import Zombie
 
 pygame.init()
 
@@ -12,8 +13,8 @@ la_font = pygame.font.SysFont("Courier", 20, True, False)
 
 info = la_font.render("Zombie Pyrty",0,(255,255,255))
 
-listarec = []
-r1 = pygame.Rect(0,0,1,1);
+zombie_list = []
+cursor = pygame.Rect(0,0,1,1);
 
 secondsint = 0
 
@@ -23,26 +24,13 @@ so.play(-1)
 last_divided = 0
 coins = 50
 
-zombie = pygame.image.load('img/zombie.png')
+zombie_surface = pygame.image.load('img/zombie.png')
 
 for x in range(25):
-    w = random.randrange(10,30)
-    h = random.randrange(10,40)
+    zombie = Zombie(screen, zombie_surface)
+    zombie_list.append(zombie)
 
-    x = random.randrange(450)
-    y = random.randrange(20,450)
-    listarec.append(pygame.Rect(x, y, w, h))
-
-while running == True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for recs in listarec:
-                if r1.colliderect(recs):
-                    #recs.width = 0
-                    #recs.height = 0
-                    listarec.remove(recs)
+while running:
 
     time.tick(20)
     #print pygame.time.get_ticks()/1000
@@ -53,12 +41,12 @@ while running == True:
 
     if (secondsint % 2) == 0 and secondsint > last_divided:
         last_divided = secondsint
-        coins = coins + 50
+        coins += 50
 
     show_coins = "$ " + str(coins)
     show_coins = la_font.render(show_coins,0,(255,255,255))
 
-    r1.center = pygame.mouse.get_pos()
+    cursor.center = pygame.mouse.get_pos()
 
     screen.fill((0,0,0))
     screen.blit(info, (20, 10))
@@ -66,12 +54,23 @@ while running == True:
     screen.blit(show_coins, (250, 10))
 
 
-    for recs in listarec:
-        recs[0] += 1
-        #pygame.draw.rect(screen,(0,200,0), recs)
-        screen.blit(transform.scale(zombie, recs[2:]), recs)
+    for zombie in zombie_list:
+        zombie.render()
 
-    pygame.draw.rect(screen,(200,20,20), r1)
+    pygame.draw.rect(screen,(200,20,20), cursor)
     pygame.display.update()
+
+
+
+    left_click = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            left_click = True
+
+    for zombie in reversed(zombie_list):
+        zombie.frame(cursor, left_click)
 
 pygame.quit()
